8a047172f4c587e6655f55a4e345d8f9b2b81305
Ren <sseuraeki@gmail.com>
2017-10-10 13:46:42 +0900

    answer1 system arguments fix added, answer3 added

---
+++ answer3.py
@@ -0,0 +1,30 @@
+import sys
+
+assert len(sys.argv) >= 3, "Usage: python answer3.py [cacheSize] [array]"
+
+size = int(sys.argv[1]) # convert string system arg to int
+
+# convert string system arg to list
+cities = sys.argv[2]
+if len(sys.argv) > 3:
+	for i in range(3, len(sys.argv)):
+		cities += sys.argv[i]
+
+cities = cities.split(",")
+cities = [x.strip("[").strip("]").strip() for x in cities]
+
+def answer3(size, arr):
+	cache = []
+	duration = 0
+
+	for i in range(len(arr)):
+		if arr[i].lower() in cache:
+			duration += 1
+		else:
+			duration += 5
+		cache.append(arr[i].lower())
+		cache = cache[-size:]
+
+	return duration
+
+print(answer3(size, cities))
